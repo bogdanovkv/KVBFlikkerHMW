6ef06e59cecdbe1af0888231dfd123f9d60913b7
Константин <bogdakos@mail.ru>
2017-12-09 18:29:18 +0300

    Пустой проект

---
+++ KVBFlikker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVBFlikker
+//
+//  Created by Константин on 09.12.17.
+//  Copyright © 2017 Konstantin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVBFlikker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVBFlikker
+//
+//  Created by Константин on 09.12.17.
+//  Copyright © 2017 Konstantin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
